5411c76335c6236b2553e0a531cdd3a8bfaa9ae4
bas <bas@ya.ru>
2020-09-18 15:23:20 +0300

    Step 3

--- README.md
+++ README.md
@@ -3,3 +3,22 @@ https://brekot.github.io/ava-webpack/index.html<br>
 <hr>
 
 Папка /docs - для разработчика. Содержит готовые файлы верски. Остальные файлы и папки служебные.
+
+<hr>
+
+<b>Подключены в JS:</b><br>
+<ul>
+    <li>jQuery</li>
+    <li>jQuery.dropdown - для всплывающих элементов (https://www.jqueryscript.net/demo/Bootstrap-style-jQuery-Drop-Down-Plugin-dropdown/)</li>
+</ul>
+
+<hr>
+
+<b>Bootstrap используются только классы:</b><br>
+<ul>
+    <li>.container</li>
+    <li>.row</li>
+    <li>.col-*</li>
+    <li>.d-*-none</li>
+    <li>.d-*-block</li>
+</ul>
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,29 @@
-/*import $ from "jquery";
+import dropdown from './jquery.dropdown.min';
 
 $(function() {
 
-});*/
+    // Выпадающие плашки
+    $('.jq-dropdown a').click(function(){
+
+        $('.jq-dropdown').hide();
+        $('.jq-dropdown-open').removeClass('jq-dropdown-open');
+    });
+
+    $('.page-counter__selector a').click(function(){
+
+        $('.page-counter__label-count').text($(this).data('val'));
+    });
+
+    // Раскрытие меню на мобиле
+    $('.btn-menu').click(function(){
+
+        $('.nav-mobile').addClass('nav-mobile_open');
+        $('body').css({'overflow': 'hidden'});
+    });
+
+    $('.nav-mobile__close').click(function(){
+
+        $('.nav-mobile').removeClass('nav-mobile_open');
+        $('body').css({'overflow': 'auto'});
+    });
+});
