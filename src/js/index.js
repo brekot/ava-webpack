5de016f2d9c3b3f69377fecf329e0d006fb370ec
bas <bas@ya.ru>
2020-09-21 15:12:30 +0300

    Filter tmp

--- src/js/index.js
+++ src/js/index.js
@@ -26,4 +26,48 @@ $(function() {
         $('.nav-mobile').removeClass('nav-mobile_open');
         $('body').css({'overflow': 'auto'});
     });
+
+
+
+    $('.search-line__inner').click(function(){
+
+        $('.filter-block').toggleClass('filter-block_open');
+    });
+
+    $(document).mouseup(function (e){
+
+		var div = $('.search-line__panel');
+
+		if (!div.is(e.target) && div.has(e.target).length === 0)
+        {
+			$('.filter-block').removeClass('filter-block_open');
+		}
+	});
+    
+    $('.filter-block__line').click(function(){
+
+        $(this).children('input').focus();
+    });
+
+    $(".filter-block__input").keyup(function() {
+
+        if ($(this).val() !== '')
+        {
+            $(this).parent('.filter-block__line').addClass('filter-block__line_set');
+        }
+        else
+        {
+            $(this).parent('.filter-block__line').removeClass('filter-block__line_set');
+        }
+    });
+
+    $('.filter-select-dropdown__checkbox').on('change', function(){
+
+        $(this).parents('.jq-dropdown').prev().find('.search-line__label').remove();
+        
+        $(this).parents('.jq-dropdown').find('.filter-select-dropdown__checkbox:checked').each(function(){
+
+            $(this).parents('.jq-dropdown').prev().prepend('<a href="javascript:;" class="search-line__label">' + $(this).val() + '<span class="search-line__label-icon font-md"></span></a>')
+        });
+    });
 });
